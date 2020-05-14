a972521a32de63199911cf55d0f0a628fb00230d
yu.zou <yu.zou@ly.com>
2020-05-14 21:16:52 +0800

    增加url

--- layuiadmin/config.js
+++ layuiadmin/config.js
@@ -10,6 +10,7 @@
 layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
   exports('setter', {
     container: 'LAY_app' //容器ID
+    ,baseUrl: 'https://localhost:44314/api/'
     ,base: layui.cache.base //记录静态资源所在路径
     ,views: layui.cache.base + 'tpl/' //动态模板所在目录
     ,entry: 'index' //默认视图文件名
